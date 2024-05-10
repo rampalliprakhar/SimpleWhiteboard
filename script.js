1b8ac7622d0a016a646ff7ad40bcb1e48efe9184
rampalliprakhar <114451266+rampalliprakhar@users.noreply.github.com>
2024-05-10 00:37:09 -0400

    Refactored the whiteboard with new functionalities

--- script.js
+++ script.js
@@ -1,68 +1,80 @@
-let canvas = document.getElementById('canvas');
+// DOM
+const canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-let sizeSlide = document.getElementById("cursorSize");
-let brushSize = (ctx.lineWidth);
+// Body
+const body = document.querySelector('body');
+// Size of the pen
+//let brushSize = ctx.lineWidth;
+var brushSize = 5;
+// Drawing tools
+let tools = 'pen';
+// Background color variable
+var colorChoice ='';
+// Coordinates
+let endX = 0;
+let endY = 0;
+// Array of variable shapes
+let shapes = [];
+// Drag objects along with its value
+let draggable = false;
+let dragStartX, dragStartY;
+
+// Body background change by user choice
+var bgColor = document.getElementById("usercolor");
+
+bgColor.addEventListener("input", function(){
+  colorChoice = bgColor.value;
+  body.style.backgroundColor = colorChoice;
+}, false);
+
+
 
 window.addEventListener('load', ()=>{
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext('2d');
-
     //Resizing
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-
     // variables
     let paint = false;
-
-    function startPosition(){
+    // starting point of the stroke
+    function startPosition(e){
         paint = true;
         draw(e);
     }
+    // ending point of the stroke
     function endPosition(){
         paint = false;
         ctx.beginPath();
     }
+    // drawing function
     function draw(e){
         if(!paint) return;
-        //ctx.lineWidth = 12;
         brushSize;
         ctx.lineCap = "round";
-
-        ctx.lineTo(e.clientX, e.clientY);
+        ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
+        ctx.moveTo(e.offsetX, e.offsetY);
     }
     // EventListeners
     canvas.addEventListener("mousedown", startPosition);
     canvas.addEventListener("mouseup", endPosition);
     canvas.addEventListener("mousemove", draw);
 });
+// Erase the whiteboard
 function erase(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
 }
+// slider value
+document.getElementById("slider").oninput = function() {
+    paint = null;
+    brushSize = document.getElementById("slider").value;
+    document.getElementById("sliderMax").innerHTML = brushSize;
+    ctx.lineWidth = brushSize;
+}; 
+// Change the color type
 function changeColorType(color, colorName) {
     ctx.strokeStyle = color;
     document.getElementById('colorDropdownBtn').textContent = colorName;
 }
-// Have to make changeShape() functionality work better in the future//
-/*
-function changeShape(){
-    canvas.add(rectangleDraw(e));
-}
-const rectangleDraw = (e) => {
-    ctx.strokeRect(e.clientX, e.clientY, prevMouseX - e.clientX, prevMouseY - e.clientY);
-}
-const triangleDraw = (e) => {
-    ctx.beginPath();
-    ctx.moveTo(prevMouseX, prevMouseY);
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.lineTo(prevMouseX * 2 - e.clientX, e.clientY);
-    ctx.closePath();
-}
-const circleDraw = (e) => {
-    ctx.beginPath();
-    let radius = Math.sqrt(Math.pow(prevMouseX - e.clientX), 2) + (Math.pow(prevMouseY - e.clientY),2);
-    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
-}*/
-sizeSlide.addEventListener("change", () => brushSize = sizeSlide.value());
